4d41cf6ccc491ae843f3f222244f1cb3add379dc
iiisc <63256807+iiisc@users.noreply.github.com>
2024-02-12 09:50:58 +0100

    Update Arbetsprov2.cs
    asd

--- Arbetsprov2/Arbetsprov2.cs
+++ Arbetsprov2/Arbetsprov2.cs
@@ -36,13 +36,31 @@ class Program
     }
     static void Main()
     {
-        StreamReader reader = new(@"C:\Users\Fractal ERA\Desktop\Vitec\Arbetsprov2\input.txt");
-        string input = reader.ReadToEnd();
+        //StreamReader reader = new(@"C:\Users\Fractal ERA\Desktop\Vitec\Arbetsprov2\input.txt");
+        //string input = reader.ReadToEnd();
 
-        string testString = "abc<abc!>abc";
+        string testString = " { } { }";
         Console.WriteLine("Original: {0}", testString);
+        //Console.WriteLine("Modified: {0}", NullifyJunk(testString));
 
-        Console.WriteLine("Modified: {0}", NullifyJunk(testString));
+        bool groupOpen = false;
+        //int totalGroups = 0;
+        //int points = 0;
+        //int openToken = 0;
+        //int closeToken = 0;
+
+        int i = 0;
+        foreach (var token in testString) // Iterates over input-string
+        {
+            i++;
+            // If we find start of group while no group is open
+            if (token == '{' && !groupOpen){
+                groupOpen = true;
+                Console.WriteLine(token);
+                //points += countGroup(); // Räkna antalet grupper i den öppna gruppen. Ska returnera när gruppen stängs.
+
+            }
+        }
 
 
     }
